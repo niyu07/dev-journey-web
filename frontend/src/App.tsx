3796d5afaab34305e6344988398220b8586185c7
niyu07 <niyu98194@gmail.com>
2025-11-01 20:44:53 +0900

    feat: Clockコンポーネントを追加し、現在の時間と日付を表示する機能を実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,13 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { Clock } from './features/Clock/Clock';
+import './App.css';
 
 function App() {
-  const [message, setMessage] = useState<string>("Loading...");
-
-  useEffect(() => {
-    fetch("http://localhost:8000/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => setMessage("Error: " + err));
-  }, []);
-
   return (
-    <div>
-      <h1>Frontend + Backend</h1>
-      <p>{message}</p>
-    </div>
+    <>
+      <Clock />
+    </>
   );
 }
 
 export default App;
+
